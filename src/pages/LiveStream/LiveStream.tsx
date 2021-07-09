7408db015cea2e71623661a2798f9bc286647a41
Recep Öztürk <recep.fed@gmail.com>
2021-07-09 18:47:15 +0300

    Video wall's header created

--- src/pages/LiveStream/LiveStream.tsx
+++ src/pages/LiveStream/LiveStream.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Breadcrumb,
   Button,
@@ -6,11 +7,14 @@ import {
   NavigationItem,
   VideoCard,
   StatusWithBadge,
-  Sidebar
+  Sidebar,
 } from "@kerberos-io/ui";
 import "./livestream.scss";
 
 export default function LiveStream() {
+  const [state, setState] = React.useState({
+    showVideoWall: false,
+  });
   return (
     <>
       <div className="header">
@@ -19,7 +23,13 @@ export default function LiveStream() {
           level1={"Get instant overview of all your online cameras"}
         >
           <p className="item">Video wall</p>
-          <Icon label="toggle-off" />
+          <span
+            onClick={() =>
+              setState((preS) => ({ ...preS, showVideoWall: true }))
+            }
+          >
+            <Icon label="toggle-off" />
+          </span>
           <Icon label="alerts-notify" />
         </Breadcrumb>
       </div>
@@ -41,38 +51,51 @@ export default function LiveStream() {
           <p className="item">By Site</p>
         </div>
       </div>
-      <div className="video-wall-embedded ">
-        <VideoCard
-          label="Hailway"
-          videoSource={"/images/video.png"}
-          headerStatus={"live"}
-          videoStatus={"recording"}
-        />
-        <VideoCard
-          label="Mailway"
-          videoSource={"/images/video.png"}
-          headerStatus={"event-detected"}
-          videoStatus={"recording"}
-        />
-        <VideoCard
-          label="Railway"
-          videoSource={"/images/video.png"}
-          headerStatus={"offline"}
-          videoStatus={"recording"}
-        />
-        <VideoCard
-          label="Sailway"
-          videoSource={"/images/video.png"}
-          headerStatus={"live"}
-          videoStatus={"recording"}
-        />
-      </div>
-      
-      <div className="video-wall-fs">
-       <div className="header"> 
-
-       </div>
-      </div>
+      {!state.showVideoWall ? (
+        <div className="video-wall-embedded ">
+          <VideoCard
+            label="Hailway"
+            videoSource={"/images/video.png"}
+            headerStatus={"live"}
+            videoStatus={"recording"}
+          />
+          <VideoCard
+            label="Mailway"
+            videoSource={"/images/video.png"}
+            headerStatus={"event-detected"}
+            videoStatus={"recording"}
+          />
+          <VideoCard
+            label="Railway"
+            videoSource={"/images/video.png"}
+            headerStatus={"offline"}
+            videoStatus={"recording"}
+          />
+          <VideoCard
+            label="Sailway"
+            videoSource={"/images/video.png"}
+            headerStatus={"live"}
+            videoStatus={"recording"}
+          />
+        </div>
+      ) : (
+        <div className="video-wall-fs">
+          <div className="vw-header">
+            <a className="brand-logo">
+              <img src={"/images/header-minimal-logo-36x36.svg"} />
+              <h4>{"Kerberos Hub Lite"}</h4>
+            </a>
+            <div className="grid-list">
+              <p className="item"> Grid size</p>
+              <div className="numbers">
+                <button>4</button>
+                <button>6</button>
+                <button>9</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
