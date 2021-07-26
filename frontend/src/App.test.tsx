ca6826542ce9c73e284039b9d7a3a79cc0a6d727
cedricve <cedric@verstraeten.io>
2021-07-26 15:39:47 +0200

    fix test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import {Router} from 'react-router-dom'
+import {createMemoryHistory} from 'history'
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App>
+          Kerberos.io is awesome
+      </App>
+    </Router>);
+  const linkElement = screen.getByText(/Kerberos.io is awesome/i);
   expect(linkElement).toBeInTheDocument();
 });
