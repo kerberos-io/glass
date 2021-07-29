cacd98553c844a0a5b94c17a57d41d7c2fe803ac
Recep Öztürk <recep.fed@gmail.com>
2021-07-29 09:58:40 +0300

    Starting cameras page

--- frontend/src/pages/Cameras/Cameras.tsx
+++ frontend/src/pages/Cameras/Cameras.tsx
@@ -1,10 +1,19 @@
 import { Breadcrumb, Button } from "@kerberos-io/ui";
-
+import "./cameras.scss"
 export default function Cameras() {
   return (
+    <div id="page-cameras">
+
     <Breadcrumb
       title={"Cameras"}
       level1={"Setup and manage your cameras"}
-    ></Breadcrumb>
+    >
+      <Button icon="plus-circle" label="Add Camera" type="default"/>
+      <div id="flag-button">
+       <Button  icon="flag" label="" type="neutral"/>
+      </div>
+    </Breadcrumb>
+    </div>
+
   );
 }
